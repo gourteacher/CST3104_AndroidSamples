0772a6769c6e2e56ebdf4eab88594c29c7818edb
Samira Ouaaz <souaaz@yahoo.com>
2022-02-12 21:01:54 -0500

    Week 5 - ListView skeleton

--- app/src/main/java/com/cst3104/samples/FirstActivity.java
+++ app/src/main/java/com/cst3104/samples/FirstActivity.java
@@ -1,4 +1,4 @@
-package com.cst2335.exercises;
+package com.cst3104.samples;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +9,7 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
