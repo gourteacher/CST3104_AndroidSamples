3f1519069493824b28583c3c0cb702e873eeeb77
gourteacher <ouaazs@algonquincollege.com>
2022-09-27 18:51:51 -0400

    Simplified Implicit Intents Example

--- app/src/main/java/com/cst3104/samples/ActivityIntentExamples.java
+++ app/src/main/java/com/cst3104/samples/ActivityIntentExamples.java
@@ -1,12 +1,19 @@
 package com.cst3104.samples;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import static android.Manifest.permission.CALL_PHONE;
 
 public class ActivityIntentExamples extends AppCompatActivity {
+
+    static private final String TAG = "ActivityIntentExamples";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +28,13 @@ public class ActivityIntentExamples extends AppCompatActivity {
             intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"email@example.com"});
             intent.putExtra(Intent.EXTRA_SUBJECT, "subject here");
             intent.putExtra(Intent.EXTRA_TEXT, "body text");
-            startActivity(intent);
 
+            try {
+                startActivity(intent);
+            }
+            catch(ActivityNotFoundException ex) {
+                Log.e(TAG, "Could not find an email application");
+            }
         });
 
         //This shows how to use Android's default web view app to view a web page:
@@ -32,9 +44,12 @@ public class ActivityIntentExamples extends AppCompatActivity {
             String url = "http://www.algonquincollege.com";
             Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData( Uri.parse(url) );
-            startActivity(i);
 
+            if (i.resolveActivity(getPackageManager() ) != null) {
+                startActivity(i);
+            }
         });
+
 
         //This shows how to use Android's default web view app to view a web page:
         temp = findViewById(R.id.makePhoneCall);
@@ -42,7 +57,14 @@ public class ActivityIntentExamples extends AppCompatActivity {
             String url ="tel:" + "6137274700";
             Intent i = new Intent(Intent.ACTION_CALL);
             i.setData( Uri.parse(url) );
-            startActivity(i);
+
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                startActivity(i);
+            } else {
+                requestPermissions(new String[]{CALL_PHONE}, 1);
+            }
+
         });
 
         Button term = findViewById( R.id.intent_return_button);
