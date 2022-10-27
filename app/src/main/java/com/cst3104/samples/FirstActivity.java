612e8b4947d636b819b6ce87d681b1832884d03f
gourteacher <ouaazs@algonquincollege.com>
2022-10-27 19:30:04 -0400

    Scenario for ANR

--- app/src/main/java/com/cst3104/samples/FirstActivity.java
+++ app/src/main/java/com/cst3104/samples/FirstActivity.java
@@ -2,70 +2,37 @@ package com.cst3104.samples;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
-    private ArrayList<String> elements = new ArrayList<>(  );
-    MyListAdapter myAdapter;
+    private static final String TAG = "FirstActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Your program starts here
         super.onCreate(savedInstanceState);
-
-        // setContentView loads objects onto the screen.
-        // Before this function, the screen is empty.
         setContentView(R.layout.activity_main);
 
-        ListView myList = findViewById(R.id.theListView);
-        //Line 37 is the same as lines 35 and 36:
-        //ListAdapter myAdapter = new MyListAdapter();
-        //myList.setAdapter( myAdapter);
-        myList.setAdapter( myAdapter = new MyListAdapter());
+        Button start = findViewById( R.id.startId);
 
-        Button addButton = findViewById(R.id.addButton);
-        addButton.setOnClickListener( click -> {
+        TextView tv = findViewById(R.id.textViewId);
 
-            Log.i(TAG, "addButton");
+        start.setOnClickListener(view -> {
+            int i = 0;
+            while (i <= 20) {
+                try {
+                    Thread.sleep(1000);
+                    i++;
+                }
+                catch (Exception e) {
+                    Log.e(TAG, "Error while sleeping");
+                }
+            }
+            tv.setText("Button Pressed");
         });
-
-    }
-
-    private class MyListAdapter extends BaseAdapter {
-
-        public int getCount() { return elements.size();}
-
-        public Object getItem(int position) { return "This is row " + position; }
-
-        public long getItemId(int position) { return (long) position; }
-
-        public View getView(int position, View old, ViewGroup parent)
-        {
-            LayoutInflater inflater = getLayoutInflater();
-
-            //make a new row:
-            View newView = inflater.inflate(R.layout.row_layout, parent, false);
-
-            //set what the text should be for this row:
-            TextView tView = newView.findViewById(R.id.textGoesHere);
-            tView.setText( getItem(position).toString() );
-
-            //return it to be put in the table
-            return newView;
-        }
     }
 }
 
