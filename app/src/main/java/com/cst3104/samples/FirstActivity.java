be3cc32ab0b1cded9d7a83a634dddcbfd92c264a
gourteacher <ouaazs@algonquincollege.com>
2022-11-06 11:34:30 -0500

    Options Menu Example

--- app/src/main/java/com/cst3104/samples/FirstActivity.java
+++ app/src/main/java/com/cst3104/samples/FirstActivity.java
@@ -1,16 +1,13 @@
 package com.cst3104.samples;
 
-import android.os.AsyncTask;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -18,71 +15,43 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MyHTTPRequest req = new MyHTTPRequest();
-        req.execute("https://api.publicapis.org/entries");  //Type 1
     }
 
-    //Type1     Type2   Type3
-    private static class MyHTTPRequest extends AsyncTask< String, Integer, String>
-    {
-        static private final String TAG = "MyHTTPRequest";
-
-        //Type3                Type1
-        public String doInBackground(String ... args)
-        {
-            try {
-
-                //create a URL object of what server to contact:
-                URL url = new URL(args[0]);
-
-                //open the connection
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                //wait for data:
-                InputStream response = urlConnection.getInputStream();
-
-                //JSON reading:
-                //Build the entire string response:
-                BufferedReader reader = new BufferedReader(new InputStreamReader(response, StandardCharsets.UTF_8), 8);
-                StringBuilder sb = new StringBuilder();
-
-                String line = null;
-                while ((line = reader.readLine()) != null)
-                {
-                    sb.append(line).append("\n");
-                }
-                String result = sb.toString(); //result is the whole string
-
-                // convert string to JSON
-                JSONObject uvReport = new JSONObject(result);
-
-                //get the double associated with "value"
-                int numEntries = uvReport.getInt("count");
-
-                publishProgress(25);
-                Thread.sleep(1000);
-                publishProgress(50);
-                Log.i(TAG, "Number of entries: " + numEntries) ;
-            }
-            catch (Exception e)
-            {
-                Log.w(TAG, "Error in doInBackground");
-            }
-            return "Done";
-        }
-
-        //Type 2
-        public void onProgressUpdate(Integer ... args)
-        {
-            Log.i(TAG, "onProgressUpdate");
-        }
-
-        //Type3
-        public void onPostExecute(String fromDoInBackground)
-        {
-            Log.i(TAG, fromDoInBackground);
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        View v = findViewById(R.id.my_rectangle);
+
+        switch(id) {
+            case R.id.red_id:
+                v.setBackgroundColor(Color.RED);
+                break;
+
+            case R.id.blue_id:
+                v.setBackgroundColor(Color.BLUE);
+                break;
+
+            case R.id.green_id:
+                v.setBackgroundColor(Color.GREEN);
+                break;
+
+            case R.id.yellow_id:
+                v.setBackgroundColor(Color.YELLOW);
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
 
