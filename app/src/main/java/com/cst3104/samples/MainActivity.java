443ef800548811f9d6a85bcf77d2bf6b9f8cdc74
gourteacher <ouaazs@algonquincollege.com>
2023-10-31 19:24:10 -0400

    Clean up the main branch.

---
+++ app/src/main/java/com/cst3104/samples/MainActivity.java
@@ -0,0 +1,23 @@
+package com.cst3104.samples;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.cst3104.samples.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+
+}
+
