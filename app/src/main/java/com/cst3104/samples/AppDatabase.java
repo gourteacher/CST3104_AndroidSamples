c9ed8b968437a14783f7965d3af573ccd87c8b91
gourteacher <ouaazs@algonquincollege.com>
2023-11-06 18:13:21 -0500

    Room Database example

---
+++ app/src/main/java/com/cst3104/samples/AppDatabase.java
@@ -0,0 +1,26 @@
+package com.cst3104.samples;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.cst3104.samples.PersonDAO;
+
+
+@Database(entities = {Person.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase minstance;
+    private static final String DB_NAME = "person_db";
+    public abstract PersonDAO getPersonDAO();
+    public static synchronized AppDatabase getInstance(Context ctx) {
+        if(minstance == null) {
+            minstance = Room.databaseBuilder(ctx.getApplicationContext(),
+                        AppDatabase.class, DB_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return minstance;
+    }
+}
