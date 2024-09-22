06a911bed9fe9602da72042b51adcad5d4954f46
gourteacher <ouaazs@algonquincollege.com>
2024-09-21 21:13:07 -0400

    Demo work

--- app/src/main/java/com/cst3104/samples/MainActivity.java
+++ app/src/main/java/com/cst3104/samples/MainActivity.java
@@ -14,16 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.i(TAG, "onCreate");
-
     }
-
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart");
+        Log. i(TAG, "onStart");
     }
 
     protected void onResume() {
@@ -53,25 +50,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy");
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Log.i(TAG, "onSaveInstanceState");
-
-        final EditText editText = findViewById(R.id.mytext);
-        CharSequence userText = editText.getText();
-        outState.putCharSequence("savedText", userText);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.i(TAG, "onRestoreInstanceState");
-
-        final EditText editText = findViewById(R.id.mytext);
-        CharSequence userText = savedInstanceState.getCharSequence("savedText");
-        editText.setText(userText);
     }
 }
