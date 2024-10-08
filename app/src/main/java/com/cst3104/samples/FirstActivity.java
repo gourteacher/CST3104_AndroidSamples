8f88bfc189e25e9c079504c730a2b33c08a63be4
gourteacher <ouaazs@algonquincollege.com>
2024-10-08 16:53:14 -0400

    Fix Warnings

--- app/src/main/java/com/cst3104/samples/FirstActivity.java
+++ app/src/main/java/com/cst3104/samples/FirstActivity.java
@@ -2,15 +2,10 @@ package com.cst3104.samples;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
-
 import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,9 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class FirstActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
-    private ArrayList<String> elements = new ArrayList<>(  );
-
+    private final ArrayList<String> elements = new ArrayList<>(  );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +27,14 @@ public class FirstActivity extends AppCompatActivity {
 
         Button addButton = findViewById(R.id.addButton);
         addButton.setOnClickListener( click -> {
-            elements.add( "This is row " + elements.size() );
+            elements.add(String.valueOf(R.string.row_added + elements.size()));
             myAdapter.notifyDataSetChanged();
             Log.i(TAG, "addButton");
         });
 
-        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                elements.remove(i);
-                myAdapter.notifyDataSetChanged();
-            }
+        myList.setOnItemClickListener((adapterView, view, i, l) -> {
+            elements.remove(i);
+            myAdapter.notifyDataSetChanged();
         });
 
     }
