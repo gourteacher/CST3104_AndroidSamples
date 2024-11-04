3133a78a785e7c8fe5507399f5a70c7e5b1e6650
gourteacher <ouaazs@algonquincollege.com>
2024-11-04 16:37:09 -0500

    Upgrade Gradle version

--- app/src/main/java/com/cst3104/samples/FirstActivity.java
+++ app/src/main/java/com/cst3104/samples/FirstActivity.java
@@ -31,25 +31,20 @@ public class FirstActivity extends AppCompatActivity {
 
         View v = findViewById(R.id.my_rectangle);
 
-        switch(id) {
-            case R.id.red_id:
-                v.setBackgroundColor(Color.RED);
-                break;
-
-            case R.id.blue_id:
-                v.setBackgroundColor(Color.BLUE);
-                break;
-
-            case R.id.green_id:
-                v.setBackgroundColor(Color.GREEN);
-                break;
-
-            case R.id.yellow_id:
-                v.setBackgroundColor(Color.YELLOW);
-                break;
-            default:
-                break;
+        if (id == R.id.red_id) {
+            v.setBackgroundColor(Color.RED);
         }
+        else if (id == R.id.blue_id) {
+            v.setBackgroundColor(Color.BLUE);
+        }
+
+        else if (id == R.id.green_id) {
+            v.setBackgroundColor(Color.GREEN);
+        }
+        else if ( id == R.id.yellow_id) {
+            v.setBackgroundColor(Color.YELLOW);
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
